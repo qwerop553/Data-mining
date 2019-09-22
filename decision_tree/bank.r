1096229d7e26edf6eb8b42388d682b1f4f6322b1
qwerop553 <qwerop553@gmail.com>
2019-09-22 17:46:39 +0900

    일반화 테스트 부분 추가

--- decision_tree/bank.r
+++ decision_tree/bank.r
@@ -23,3 +23,23 @@ print(fitTree)
 plot(fitTree)
 text(fitTree)
 prp(fitTree, type=4, extra=2, digits=3)
+
+# 평가
+predlmsi <- predict(object = fitTree, newdata = bank, type = 'class') # 0, 1의 값을 원함
+
+table(bank$PersonalLoan, predlmsi)
+
+# 일반화 능력 평가
+index <- sample.int(nrow(bank), nrow(bank) * 0.7)
+
+train <- bank[index,]
+test <- bank[-index,]
+
+fitTree2 <- rpart(formula = PersonalLoan~., train)
+p <- predict(object = fitTree2, newdata = test, type = 'class')
+table(test$PersonalLoan, p)
+"
+      0    1
+  0 1350    9
+  1    9  132
+"
