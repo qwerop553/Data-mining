00b17f0924039d44b1cca82532fa381d4ed4303f
qwerop553 <qwerop553@gmail.com>
2019-10-17 11:45:37 +0900

    10월 7일 데이터마이닝 수업

--- k_means/teens_k_means.R
+++ k_means/teens_k_means.R
@@ -2,6 +2,8 @@
 # 십대 SNS 페이지의 텍스트를 이요하여 스포츠, 종교, 음악과 같은 공통 관심을 공유하는 그룹을 찾을 수 있음
 # 데이터 수집: 데이터는 SNS 사이트를 크롤링해서 가져옴
 # 데이터 탐색 및 준비
+library(ggplot2)
+library(tidyverse)
 
 teens <- read.csv("C:/data_mining/k_means/snsdata.csv")
 
@@ -13,7 +15,7 @@ table(teens$gender, useNA='ifany') # NA 도 집계
 
 summary(teens$age) # summary는 원래 집계
 
-teens$age <- ifelse(teens$age >= 13 &teens$age, teens$age, NA)
+teens$age <- ifelse(teens$age >= 13 &teens$age < 20, teens$age, NA)
 
 summary(teens$age)
 
@@ -49,3 +51,85 @@ teens$no_gender <- ifelse(is.na(teens$gender), 1, 0)
 # 확인 
 table(teens$female);table(teens$male);table(teens$no_gender);
 2724 + 5222 == 7946
+
+# 연령의 평균, 17.25243 세
+mean(teens$age, na.rm = T)
+
+# 성별의 분포 
+g <- ggplot(teens, aes(age))
+g + geom_density(aes(fill=gender), color=NA, width = 0.5, alpha=0.3) 
+
+teens$age_fl <- floor(teens$age)
+table(teens$age_fl, teens$gender)
+table(teens$age_fl, teens$gender, useNA='ifany')
+
+# 졸업 연도별 평균나이
+teens %>% group_by(gradyear) %>% summarize(me=mean(age, na.rm=T))
+aggregate(age~gradyear, teens, FUN = function(x) mean(x, na.rm=T))
+aggregate(age~gradyear, teens, mean, na.rm=T)
+aggregate(age~gradyear, teens, mean)
+
+#
+# 연령 결측치 처리
+# 졸엽년도가 같은 사람들의 나이로 대체
+#
+# gradyear 별로 age의 mean 값을 구한다.
+# ave: 그룹의 대표값을 형성하는 함수
+# ave: 그룹의 대표값을 구할 특성, 그룹, 대표값을 어떤 통계로 할건지)
+# 원본데이터의 차원을 그대로 유지
+# 즉 age가 1000개의 값을 들어오면 ave_age도 1000개의 값을 생성
+# ave(teens$age, teens$gradyear, FUN=function(x) mean(x, na.rm = TRUE))
+
+# 졸업 연도의 결측치 확인
+table(teens$gradyear, useNA='ifany') # 없음
+
+# 연령 결측치 처리코드
+table(teens$age, useNA='ifany')
+ave_age <- ave(teens$age, teens$gradyear, FUN=function(x) mean(x, na.rm = TRUE)) 
+# mean의 옵션을 주기 위해 FUN 인자로 mean을 전달 
+table(ave_age) #넣기 전에 확인 
+teens$age <- ifelse(is.na(teens$age), ave_age, teens$age)
+
+# 스케일 조정
+#
+# => 데이터를 정규화시켜줌
+# 극과 극의 관계.. x가 (0~100)값을 갖고,  y가 (0~1)의 값을 갖는다면,
+# 0과 100ㅔ 비해 0 과 1이 단순히 유클리디언 디스턴스로 계산을 하면,
+# 스케일이 큰 쪽(값의 범위가 큰 쪽으로)의 영향을 훨씬 크게 받기 때문에,
+# 스케일을 조정하여 비슷하게 만들어줘야 합니다.
+# ex) 몸무게 10kg, 키 150, 160.. 
+#
+# 정규화 방법
+# Xs = (X - mean) / s.d => 정규분포를 가정하고, 표준정규분포를 따르도록 정규화함
+# Xs = (X - mean) / (max - min) => 모든 값을 -0.5, 0.5 사이의 값으로 정규화
+# Xs = (X - min)  / (max - min) => 모든 값을 0, 1 사이의 값으로 정규화함
+#
+# 첫번째는 이상치를 가지고 있을 때,
+# 두번째와 세번째는 이상치가 없을 때 사용하는 게 유용하다
+#
+
+interests <- teens[5:40]
+interests_z <- as.data.frame(lapply(interests, scale))
+
+set.seed(1234)
+teen_clusters <- kmeans(interests_z, 5)
+
+teen_clusters
+teen_clusters$size
+teen_clusters$centers
+
+teens$cluster <- teen_clusters$cluster
+
+# 처음 5개의 데이터만 살펴보기
+teens[1:5, c("cluster", "gender", "age", "friends")]
+
+# 각 클러스터별로 연령 평균 구하기
+aggregate(data = teens, age ~ cluster, mean)
+
+# 각 클러스터별로 여성의 비율 구하기
+aggregate(data = teens, female ~ cluster, mean)
+
+# 각 클러스터별로 친구 수 구하기
+aggregate(data = teens, friends ~ cluster, mean)
+
+aggregate(data = teens, soccer ~ cluster, mean)
