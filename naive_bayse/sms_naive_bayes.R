d5ab048d6ed032b1ad392bea6a81516df387c5d3
qwerop553 <qwerop553@gmail.com>
2019-11-15 14:50:14 +0900

    11월15일

--- naive_bayse/sms_naive_bayes.R
+++ naive_bayse/sms_naive_bayes.R
@@ -2,57 +2,82 @@ library(tidyverse)
 library(caret)
 library(tm) # library for text mining
 library(extrafont)
+library(SnowballC) # stemming(learning, learned => learn)
+library(wordcloud)
+library(e1071)
+library(gmodels)
 
 setwd("./naive_bayse")
 sms_raw <- read.csv("sms_spam.csv", encoding="UTF-8")
 
-sms_raw$type <- factor(sms_raw$type)
+table(sms_raw$type)
 
-train_index <- caret::createDataPartition(sms_raw$type, p=0.75, list=FALSE) 
-# sms_raw$type이 균일하게 분포하도록 row number를 뽑아 준다.
-# p는 어느 정도의 비율을 추출할지를 나타낸다.
-
-
-# train data
-sms_raw_train <- sms_raw[train_index,]
-
-# test data
-sms_raw_test <- sms_raw[-train_index,]
-
-# create train corpus and refine, and make Document Term Matrix
-sms_corpus_clean_train <- Corpus(VectorSource(sms_raw_train$text)) %>%
+# create train corpus and refine
+sms_corpus_clean <- Corpus(VectorSource(sms_raw$text)) %>%
                           tm_map(content_transformer(tolower)) %>%
                           tm_map(removeNumbers) %>%
                           tm_map(removeWords, stopwords(kind='en')) %>%
                           tm_map(removePunctuation) %>%
-                          tm_map(stripWhitespace)
-                    
-sms_dtm_train <- DocumentTermMatrix(sms_corpus_clean_train)                  
-sms_dict <- findFreqTerms(sms_dtm_train, lowfreq=5)
-sms_dtm_train <- DocumentTermMatrix(sms_corpus_clean_train, list(dictionary=sms_dict))
+                          tm_map(stripWhitespace) %>% 
+                          tm_map(stemDocument)
 
-# create train corpus and refine, and make Document Term Matrix
-sms_corpus_clean_test <- Corpus(VectorSource(sms_raw_test$text)) %>%
-                          tm_map(content_transformer(tolower)) %>%
-                          tm_map(removeNumbers) %>%
-                          tm_map(removeWords, stopwords(kind='en')) %>%
-                          tm_map(removePunctuation) %>%
-                          tm_map(stripWhitespace)
+as.character(sms_corpus_clean[[1]])
 
-sms_dtm_test <- DocumentTermMatrix(sms_corpus_clean_test, list(dictionary=sms_dict))
+# wordcloud for ham, spam
+wordcloud(sms_corpus_clean, min.freq=50, random.order=FALSE)
+
+ham <- subset(sms_raw, sms_raw$type=='ham')
+spam <- subset(sms_raw, sms_raw$type=='spam')
+
+wordcloud(ham$text, min.freq=50, random.order=FALSE)
+wordcloud(spam$text, min.freq=10, random.order=FALSE)
+wordcloud(spam$text, max.words=40, random.order=FALSE)
+
+# create document term matrix
+sms_dtm <- DocumentTermMatrix(sms_corpus_clean)
+inspect(sms_dtm[1:10, 1:30])
+
+sms_freq_words <- findFreqTerms(sms_dtm, 5)
+str(sms_freq_words)
+sms_dtm_freq <- sms_dtm[,sms_freq_words]
+dim(sms_dtm_freq)
+dim(sms_dtm)
 
 # labeling function
-convert_corpus <- function(x){
+convert_counts <- function(x){
   x <- ifelse(x > 0 , 1, 0)
   x <- factor(x, levels=c(0, 1), labels=c("Absent", "Present"))
 }
 
 # label
-sms_train <- sms_dtm_train %>% apply(MARGIN=2,  FUN=convert_corpus)
-sms_test <- sms_dtm_test %>% apply(MARGIN=2, FUN=convert_corpus)
+sms_dtm_convert <- apply(sms_dtm_freq, 2, convert_counts)
+
+# data partitioning
+train_index <- createDataPartition(sms_raw$type, p=0.75, list=FALSE)
+
+sms_dtm_train <- sms_dtm_convert[train_index,]
+sms_raw_train <- sms_raw[train_index,]
+
+sms_dtm_test <- sms_dtm_convert[-train_index,]
+sms_raw_test <- sms_raw[-train_index,]
 
 # Modeling
-# trainControl : Control the computational nuances of the train function
+## Naive Bayes at e1071
+m <- naiveBayes(sms_dtm_train, sms_raw_train$type)
+
+m <- naiveBayes(sms_dtm_train, sms_raw_train$type, laplace = 2)
+
+# Evaluation
+p <- predict(m, sms_dtm_test, type='class')
+table(sms_raw_test$type, p)
+CrossTable(p, sms_raw_test$type,
+           prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE,
+           dnn = c('predicted', 'actual'))
+
+
+## Naive Bayes at caret
+
+### trainControl : Control the computational nuances of the train function
 ctrl <- trainControl(method='cv', number=10) # cross validation 10 times. repeat = 3 이 안되네..
 
 sms_nb_mod <- train(sms_train, sms_raw_train$type, method='nb', trControl=ctrl)
