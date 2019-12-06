b50f2f7b346e717bc0dc990098b83c858b0d388b
qwerop553 <44725349+qwerop553@users.noreply.github.com>
2019-12-06 14:32:11 +0900

    finished

--- regression/regression.R
+++ regression/regression.R
@@ -8,4 +8,24 @@ reg <- function(y, x){
 
 challenger <- read.csv("challenger.csv")
 
+# 부모의 키와 자식의 키의 상관관계
+# install.packages("UsingR")
+library(UsingR) # UsingR 패키지 => 데이터가 모여있는 패키지
+str(galton)
 
+par(mfrow = c(1, 2))
+hist(galton$child, col='blue', breaks=100)
+hist(galton$parent, col='blue', breaks=100)
+
+cor(galton$child, galton$parent)
+
+cor.test(galton$child, galton$parent)
+
+lm.model = lm(child~parent, data = galton)
+lm.model
+
+summary(lm.model)
+
+plot(child~parent, data=galton)
+
+abline(lm.model, col='red')
